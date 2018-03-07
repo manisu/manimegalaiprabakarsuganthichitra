9226ac522856d0578e6f145ae6cec83ecc0e58dd
manisu <35994839+manisu@users.noreply.github.com>
2018-03-07 16:11:47 +0530

    Create product.c

---
+++ product.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int num,digit,p=1;
+printf("\n enter the number");
+scanf("%d",&num);
+while(num!=0)
+{
+digit=num%10;
+num=num/10;
+p=p*digit;
+}
+printf("\n product=%d",p);
+getch();
+}
