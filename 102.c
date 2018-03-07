c605662b8eae97d4f78027ce325a7314efd4e9e0
manisu <35994839+manisu@users.noreply.github.com>
2018-03-07 16:41:44 +0530

    Update 102.c

--- 102.c
+++ 102.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 void main()
 {
-int n;
+int a;
 printf("\n Enter the number:");
-scanf("%d",&n);
-while(n%2==0)
+scanf("%d",&a);
+while(a%2==0)
 {
-printf("\n%d",(n/2));
-n=n/2;
+printf("\n%d",(a/2));
+a=a/2;
 }
-if(n%2!=0)
+if(a%2!=0)
 {
 	printf("it does not divisible by 2");
 }
