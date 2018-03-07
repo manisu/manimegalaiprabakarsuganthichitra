141c6002307595e036262719a5b4f86ef8f7b5af
manisu <35994839+manisu@users.noreply.github.com>
2018-03-07 15:51:52 +0530

    Create changednum.c

---
+++ changednum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n,a[20],i,b=0;
+printf("\n enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+b=b+1;
+if(a[i]!=b)
+{   printf("\n %d",b);
+}
+}
+getch();
+}
