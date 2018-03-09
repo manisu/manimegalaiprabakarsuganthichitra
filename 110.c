6f7794bc9ff87c29ab6665d74f925bdcc2f3f369
manisu <35994839+manisu@users.noreply.github.com>
2018-03-09 16:46:00 +0530

    Create 110.c

---
+++ 110.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+ long int n,sum=0;
+ int i;
+ scanf("%ld", &n);
+ for (i = 1; i <=n; ++i)
+ {
+ 	sum=sum+i;
+ }
+
+            printf("%ld",sum);
+ 
+    getch();
+    }
